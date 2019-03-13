ff3455feec21f16641b0caba6fdb44ad8afff845
larissanne <larissadavies31@gmail.com>
2019-03-13 09:47:48 +0000

    updated

---
+++ install.sh
@@ -0,0 +1,7 @@
+#create spring user
+sudo useradd -m -s /bin/bash spring
+#install java + git + maven
+sudo apt install -y openjdk-8-jre openjdk-8-jdk git
+sudo apt install -y maven
+sudo cp spring.service /etc/systemd/system
+
